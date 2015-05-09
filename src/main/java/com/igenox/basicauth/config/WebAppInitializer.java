139ac99d865be7f0944357359556009bbcf86956
nucatus <alexandru.ionita@gmail.com>
2015-05-09 04:55:10 -0400

    Added Logging
    Logging is performed by log4j with the apache commons logging wrapper that comes with Spring.
    The LOG object is instantiated using the commons logging LogFactory.

--- src/main/java/com/igenox/basicauth/config/WebAppInitializer.java
+++ src/main/java/com/igenox/basicauth/config/WebAppInitializer.java
@@ -1,5 +1,7 @@
 package com.igenox.basicauth.config;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
@@ -15,11 +17,19 @@ import java.util.Set;
  */
 public class WebAppInitializer implements WebApplicationInitializer
 {
+
+    private static final Log LOG = LogFactory.getLog(WebAppInitializer.class);
+    public static final String ROOT_PACKAGE = "com.igenox.basicauth";
+
     @Override
     public void onStartup(ServletContext servletContext) throws ServletException
     {
+        if (LOG.isDebugEnabled())
+        {
+            LOG.debug("Initialize Spring context with root scan in " + ROOT_PACKAGE);
+        }
         AnnotationConfigWebApplicationContext root = new AnnotationConfigWebApplicationContext();
-        root.scan("com.igenox.basicauth");
+        root.scan(ROOT_PACKAGE);
 
         servletContext.addListener(new ContextLoaderListener(root));
 
